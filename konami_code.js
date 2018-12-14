05d2c4295d4a0ef60300fc5e708c0e62755c5899
richardtam.ny <richardtam.ny@gmail.com>
2018-12-14 00:09:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,18 @@ const codes = [
 ];
 
 function init() {
+  let index = 0
+  const key = e.key
+
+window.addEventListener('keydown', function(event){
+  const key=parseInt(event.which);
+  console.log(event.which);
+  if(key===code[index]){
+    window.alert("Cheat Activated sucka")
+  } else
+} index=0
+}
+
+
   // your code here
 }
